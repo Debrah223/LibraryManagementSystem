fb266233bf9d3ebf707cf2fe069b4b0672a5fb37
Debrah223 <debrahmachuki21@gmail.com>
2025-04-02 19:38:26 +0300

    Included user urls in library app

--- library_app/urls.py
+++ library_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import BookListCreateView, BookDetailView, BookListView
+from .views import BookListCreateView, BookDetailView, BookListView, UserListView, UserDetailView
 
 urlpatterns = [
     path('books/', BookListCreateView.as_view(), name='book-list-create'), #For creating books
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'), # allows updating and deleting books by ID.
     path('books/all/', BookListView.as_view(), name='book-list'),  # For retrieving books
+    path('users/', UserListView.as_view(), name='user-list'),
+    path('users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
 
 ]
--- library_app/views.py
+++ library_app/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .serializers import UserSerializer
+
 
 class BookListCreateView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
@@ -18,3 +20,13 @@ class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     search_fields = ['title', 'author', 'isbn']  # to search for fields using query parameters
+
+class UserListView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]  # Anyone can create a user
+
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]  # Only authenticated users can update/delete
